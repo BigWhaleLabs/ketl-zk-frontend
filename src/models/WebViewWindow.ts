aa29ecfdbc9d13bcaf8431149345a18646bd8917
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-02-15 07:09:03 +0400

    fix unused

--- src/models/WebViewWindow.ts
+++ src/models/WebViewWindow.ts
@@ -1,10 +1,3 @@
-import Messages from 'models/Messages'
-
-interface PostMessage {
-  type: Messages
-  data?: string
-}
-
 export default interface WebViewWindow extends Window {
   ReactNativeWebView: {
     postMessage: (message: string) => void
