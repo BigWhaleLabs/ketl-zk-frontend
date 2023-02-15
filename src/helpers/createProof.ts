c98d92bf9955ad9a63c2846f45b7ae41a5cc6071
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-02-15 04:54:15 +0400

    add snarkjs as script

--- src/helpers/createProof.ts
+++ src/helpers/createProof.ts
@@ -1,6 +1,8 @@
-import * as snarkjs from 'snarkjs'
 import { getAllowMapInput } from 'helpers/getMerkleTreeProof'
 import ProofResult from 'models/ProofResult'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const snarkjs: any
 
 export default async function (token: string): Promise<ProofResult> {
   return snarkjs.groth16.fullProve(
